0eb0ecd3fa74f370592555767d8aa92302deaa3e
Yichun Bai <byc233@terpmail.umd.edu>
2024-06-21 00:03:03 -0400

    react nav bar

---
+++ client_employee/src/components/NavBar.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+import './NavBar.css';
+
+function NavBar() {
+    return (
+        <div id="navBarContainer">
+            <Link to="/profile" className='navLinks'>Home</Link>
+            <div>
+                <Link to="/profile" className="navLinks">Profile</Link>
+                |
+                <Link to="/visa" className='navLinks'>Visa Status Management</Link>
+                |
+                <Link to='/housing' className='navLinks'>Housing</Link>
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
---
+++ client_employee/src/pages/Housing.tsx
@@ -0,0 +1,5 @@
+function Housing() {
+    return <div>Housing Page</div>;
+}
+
+export default Housing;
