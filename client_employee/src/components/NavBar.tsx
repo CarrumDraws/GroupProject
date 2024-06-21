6b7960fbd5f735a6581a02a7a5b60f7f3e888fb1
Yichun Bai <byc233@terpmail.umd.edu>
2024-06-21 15:47:37 -0400

    UPDATED PATH

--- client_employee/src/components/NavBar.tsx
+++ client_employee/src/components/NavBar.tsx
@@ -5,13 +5,13 @@ import './NavBar.css';
 function NavBar() {
     return (
         <div id="navBarContainer">
-            <Link to="/employee/profile" className='navLinks'>Home</Link>
+            <Link to="/" className='navLinks'>Home</Link>
             <div>
-                <Link to="/employee/profile" className="navLinks">Profile</Link>
+                <Link to="/profile" className="navLinks">Profile</Link>
                 |
-                <Link to="/employee/visa" className='navLinks'>Visa Status Management</Link>
+                <Link to="/visa" className='navLinks'>Visa Status Management</Link>
                 |
-                <Link to='/employee/housing' className='navLinks'>Housing</Link>
+                <Link to='/housing' className='navLinks'>Housing</Link>
             </div>
         </div>
     );
