7a6d68d43d9d1a116d1209c43763cc3ac8af91c1
Hongqin <hongqin811@gmail.com>
2024-06-23 20:43:01 -0400

    sendToken redo done

--- client_hr/src/app/components/hiring/hiring.component.ts
+++ client_hr/src/app/components/hiring/hiring.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { TokenService } from 'src/app/services/token.service';
+import { FlashMessageService } from 'src/app/services/flash-message.service';
 
 @Component({
   selector: 'app-hiring',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HiringComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private tokenService: TokenService,
+    private flashMessageService: FlashMessageService
+  ) { }
+
+  isTokenPage = true;
+  statusFilter = ['pending', 'rejected', 'approved'];
+
+  tokenForm = new FormBuilder().group({
+    email: ['', [Validators.required, Validators.email]],
+    name: ['', Validators.required]
+  })
 
   ngOnInit(): void {
   }
+
+  switchPage(bool: boolean): void{
+    this.isTokenPage = bool;
+  }
+
+  onSendToken(): void{
+    if(this.tokenForm.valid){
+      const name = this.tokenForm.get('name')!.value!;
+      const email = this.tokenForm.get('email')!.value!;
+      
+      this.tokenService.sendToken(name, email).subscribe({
+        next:  (respose) => this.flashMessageService.info("Token sent."),
+        error: (error) => {
+          this.flashMessageService.warn(error.error.error)
+        },
+        complete: () => {}
+      });
+    }
+  }
+
+  employees_data = [
+    { name: "firstname last", email: "employeeOne@email.com"},
+    { name: "Youfirst yourlast", email: "employeeTwo@email.com"},
+    { name: "myfirstname mylast", email: "employeeThree@email.com"}
+  ]
 
 }
