7208cafa71a80e0221c5f40d6264c8bd948b405d
Carrum <calumchansy@gmail.com>
2024-06-25 10:47:03 -0700

    Some OPT Routes

---
+++ server/routers/OptRouter.js
@@ -0,0 +1,37 @@
+const { Router } = require("express");
+const multer = require("multer");
+const {
+  getOpt,
+  postOpt,
+  handleOpt,
+  getVisaEmployees,
+} = require("../controllers/OptController.js");
+
+const {
+  jwtValidation,
+  isHR,
+  RegisterValidation,
+  RegistrationValidation,
+} = require("../middlewares/AuthMiddleware.js");
+
+// Configure multer for memory storage
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+const optRouter = Router();
+
+optRouter.get("/", jwtValidation, getOpt);
+
+optRouter.patch("/:fileid", jwtValidation, isHR, handleOpt);
+optRouter.get("/visaholders", jwtValidation, isHR, getVisaEmployees);
+optRouter.post(
+  "/:type",
+  upload.fields([
+    { name: "fileone", maxCount: 1 },
+    { name: "filetwo", maxCount: 1 },
+  ]),
+  jwtValidation,
+  postOpt
+);
+
+module.exports = optRouter;
--- server/utils/processFile.js
+++ server/utils/processFile.js
@@ -6,10 +6,11 @@ async function processFile(employeeId, file, filetype) {
     const { key, filename } = await uploadFileToS3(file);
     const newFile = new File({
       employee_id: employeeId,
-      filekey: key,
       filename: filename,
-      notification_sent: "",
+      filekey: key,
       status: "Pending",
+      notification_sent: null,
+      feedback: null,
     });
     await newFile.save();
     console.log("Created " + filetype + " File");
