b1ad85baf26789aaf9601e69f4d952ef30400ad4
Hongqin <hongqin811@gmail.com>
2024-06-27 12:08:27 -0400

    fixed bugs,did some visa

--- client_hr/src/app/components/profile/profile.component.ts
+++ client_hr/src/app/components/profile/profile.component.ts
@@ -34,17 +34,17 @@ export class ProfileComponent implements OnInit {
 
     //get profile and profile picture
     this.profile$ = this.profileService.getProfile(employeeId);
-    this.picture$ = this.profile$.pipe(
-      switchMap(profile => {
-        if (profile?.profile?.picture) {
-          return this.fileService.getFileUrl(profile.profile.picture).pipe(
-            map(file => file.url)
-          );
-        } else {
-          return of(null);
-        }
-      })
-    );
+    // this.picture$ = this.profile$.pipe(
+    //   switchMap(profile => {
+    //     if (profile?.profile?.picture) {
+    //       return this.fileService.getFileUrl(profile.profile.picture).pipe(
+    //         map(file => file.url)
+    //       );
+    //     } else {
+    //       return of(null);
+    //     }
+    //   })
+    // );
     //get files
     this.files = this.fileService.getFilesByEmployeeId(employeeId);
   }
--- client_hr/src/app/components/visa/visa.component.ts
+++ client_hr/src/app/components/visa/visa.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Visa } from 'src/app/interface/visa';
+import { Observable, map } from 'rxjs';
+import { VisaService } from 'src/app/services/visa.service';
 
 @Component({
   selector: 'app-visa',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisaComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private visaService: VisaService
+  ) { }
+
+  visaList$: Observable<Visa[] | null> = new Observable<Visa[] | null>();
+
+  currentSelected = 0;
 
   ngOnInit(): void {
+    this.visaList$ = this.visaService.getAllOpt().pipe(
+
+      //calculate day remains for work 
+      map((response: Visa[] | null) => {
+        if (!response) {
+          return null;
+        }
+        return response.map(element => {
+          const now = new Date();
+          const endDate = new Date(element.workauth.enddate);
+          const dayRemains = Math.ceil((endDate.getTime() - now.getTime()) / (1000 * 60 * 60 * 24))
+          return {
+            ...element,
+            workauth: {
+              ...element.workauth,
+              dayRemains: dayRemains
+            }
+          };
+        });
+      })
+    );
+  }
+
+  updateSelected(index: number):void{
+    this.currentSelected = index;
   }
 
 }
