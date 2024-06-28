710e8aacfe0d8f9d99ae2462e17005470d3438bd
Carrum <calumchansy@gmail.com>
2024-06-28 09:00:05 -0700

    KathyChanges

--- client_employee/src/App.tsx
+++ client_employee/src/App.tsx
@@ -1,17 +1,22 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
-import ProtectedRoute from './utils/ProtectedRoute';
-import NavBar from './components/NavBar';
-import Profile from './pages/Profile';
-import VisaStatusManagement from './pages/VisaStatusManagement';
-import Housing from './pages/Housing';
-import Registration from './pages/Registration';
-import Login from './pages/Login';
-import Onboarding from './pages/Onboarding';
-import { ProfileProvider } from './context/ProfileContext';
-import { OnboardingProvider, useOnboarding } from './context/OnboardingContext';
+import ProtectedRoute from "./utils/ProtectedRoute";
+import NavBar from "./components/NavBar";
+import Profile from "./pages/Profile";
+import VisaStatusManagement from "./pages/VisaStatusManagement";
+import Housing from "./pages/Housing";
+import Registration from "./pages/Registration";
+import Login from "./pages/Login";
+import Onboarding from "./pages/Onboarding";
+import { ProfileProvider } from "./context/ProfileContext";
+import { OnboardingProvider, useOnboarding } from "./context/OnboardingContext";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
@@ -30,37 +35,78 @@ function MainRoutes() {
     <>
       {isLoggedIn && <NavBar />}
       <Routes>
-        <Route path='/' element={
-          isLoggedIn ?
-            <Navigate to={onboardingData?.status !== 'Accept' ? '/onboarding' : '/profile'} replace /> :
-            <Navigate to='/login' replace />
+        <Route
+          path="/"
+          element={
+            isLoggedIn ? (
+              <Navigate
+                to={
+                  onboardingData?.status !== "Accept"
+                    ? "/onboarding"
+                    : "/profile"
+                }
+                replace
+              />
+            ) : (
+              <Navigate to="/login" replace />
+            )
           }
         />
-        <Route path='/login' element={
-          !isLoggedIn ? (
-            <Login onboardingStatus={onboardingData?.status} />
-          ) : (
-            <Navigate to={onboardingData?.status !== 'Accept' ? '/onboarding' : '/profile'} replace />
-          )
-        } />
-        <Route path='/register/:token' element={<Registration />} />
+        <Route
+          path="/login"
+          element={
+            !isLoggedIn ? (
+              <Login onboardingStatus={onboardingData?.status} />
+            ) : (
+              <Navigate
+                to={
+                  onboardingData?.status !== "Accept"
+                    ? "/onboarding"
+                    : "/profile"
+                }
+                replace
+              />
+            )
+          }
+        />
+        <Route path="/register/:token" element={<Registration />} />
 
         {isLoggedIn && (
           <>
-            <Route path='/onboarding' element={
-              <ProtectedRoute>
-                <Onboarding />
-              </ProtectedRoute>
-            } />
-            <Route path='/profile' element={
-              <ProtectedRoute>
-                <ProfileProvider>
-                  <Profile />
-                </ProfileProvider>
-              </ProtectedRoute>
-            } />
-            <Route path='/visa' element={<ProtectedRoute><VisaStatusManagement /></ProtectedRoute>} />
-            <Route path='/housing' element={<ProtectedRoute><Housing /></ProtectedRoute>} />
+            <Route
+              path="/onboarding"
+              element={
+                <ProtectedRoute>
+                  <Onboarding />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <ProfileProvider>
+                    <Profile />
+                  </ProfileProvider>
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/visa"
+              element={
+                <ProtectedRoute>
+                  <VisaStatusManagement />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/housing"
+              element={
+                <ProtectedRoute>
+                  <Housing />
+                </ProtectedRoute>
+              }
+            />
           </>
         )}
       </Routes>
